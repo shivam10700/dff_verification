fa2141848b607ad4a0ed56ba263903795ffbd26d
shivam10700 <104898091+shivam10700@users.noreply.github.com>
2026-05-03 21:22:10 +0530

    Update dff.sv

--- design/dff.sv
+++ design/dff.sv
@@ -1,1 +1,16 @@
+module dff(
+  input logic clk,
+  input logic rst_n,
+  input logic d,
+  output logic q
+);
 
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+      q <= 0;
+    else
+      q <= d;
+
+  end
+
+endmodule
