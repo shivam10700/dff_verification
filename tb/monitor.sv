7104ada0036a31c060b639c81c6d9d52025b4cf7
shivam10700 <104898091+shivam10700@users.noreply.github.com>
2026-05-03 23:37:30 +0530

    Update monitor.sv

--- tb/monitor.sv
+++ tb/monitor.sv
@@ -1,1 +1,17 @@
+class monitor;
 
+    virtual dff_if vif;
+
+    function new(virtual dff_if vif);
+        this.vif = vif;
+    endfunction
+
+    task run();
+        forever begin
+            @(posedge vif.clk);
+            $display("MONITOR: d=%0d q=%0d rst_n=%0d time=%0t",
+                      vif.d, vif.q, vif.rst_n, $time);
+        end
+    endtask
+
+endclass
